532f735699cd6f85740eb3f007c9d508be652f4c
Wojciech Pirog <wojciech.pirog@polcode.net>
2019-02-12 08:58:09 +0100

    store model and table seeder

---
+++ app/Store.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+final class Store extends Model
+{
+    const TYPE_WOOCOMMERCE = 1;
+    const TYPE_MAGENTO = 2;
+    
+    protected $fillable = [
+        'name', 'base_url', 'type_id'
+    ];
+    
+    public $timestamps = false;
+}
---
+++ database/seeds/StoresTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Store;
+use Illuminate\Database\Seeder;
+
+final class StoresTableSeeder extends Seeder
+{
+    /**
+     * Stores to import.
+     * 
+     * @return  array
+     */
+    protected function stores()
+    {
+        return [
+            [
+                'name' => 'Demo WooCommerce Store',
+                'base_url' => 'http://woocommerce.local',
+                'type_id' => Store::TYPE_WOOCOMMERCE
+            ]
+        ];
+    }
+    
+    public function run()
+    {
+        foreach ($this->stores() as $store) {
+            Store::firstOrCreate($store);
+        }
+    }
+}
